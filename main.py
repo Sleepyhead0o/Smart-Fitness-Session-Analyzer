9a292fddabbc989ae4db80379fc3e65a8541d606
celinajasund <celina@jasund.org>
2026-09-21 21:13:19 +0200

    main file update

---
+++ main.py
@@ -0,0 +1,11 @@
+class Reference:
+    def __init__(
+        self,
+        resting_heart_rate,
+        normal_temp,
+        normal_skin_response
+    ):
+        self.resting_heart_rate = resting_heart_rate # bpm
+        self.normal_temperature = normal_temp
+        self.normal_skin_response = normal_skin_response
+
